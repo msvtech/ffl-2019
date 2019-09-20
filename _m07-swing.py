d3a26b034fa939a3e4228e5006a7018a8542d812
Dylan <dylanvanwinkle@gmail.com>
2019-09-20 07:41:27 -0700

    dylan updates 2019-09-19

--- _m07-swing.py
+++ _m07-swing.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env micropython
 
 from ev3dev2.motor import LargeMotor, MediumMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C, OUTPUT_D, SpeedPercent, MoveTank
-from ev3dev2.sensor.lego import GyroSensor, ColorSensor
+# from ev3dev2.sensor.lego import GyroSensor, ColorSensor
 import time
 tank_drive = MoveTank(OUTPUT_A, OUTPUT_B)
 front_motor = MediumMotor(OUTPUT_C)
-top_motor = MediumMotor(OUTPUT_D)
+# top_motor = MediumMotor(OUTPUT_D)
 
 ratio_degrees_to_inches = 360 / 8.44
 rotate90 = 137 / 90.0
@@ -28,5 +28,12 @@ tank_drive.on_for_degrees(SpeedPercent(100), SpeedPercent(100), ratio_degrees_to
 # Drive forward
 # tank_drive.on_for_degrees(SpeedPercent(30), SpeedPercent(30), ratio_degrees_to_inches * 16, brake=True)
 
+<<<<<<< HEAD
 #! Turn left 45 degrees
 # tank_drive.on_for_degrees(SpeedPercent(20), SpeedPercent(-20), rotate90 * 45)
+=======
+# Turn left 45 degrees
+tank_drive.on_for_degrees(SpeedPercent(20), SpeedPercent(-20), rotate90 * 45)
+
+# front_motor.on_for_degrees(SpeedPercent(100), 5 * 360)
+>>>>>>> master
--- introTeam.py
+++ introTeam.py
@@ -6,3 +6,7 @@ import time
 
 sound = Sound()
 sound.speak('Welcome to the Sage Canyon Lego Builders. We are Saira, Pallavi, Dylan, Droov, Erik, and Kiran. We are going to kick your butt! Lets go!')
+
+
+
+# in. = 36 foward 90 degree turn right , 7in foward, 90 degree turn left, 5in foward
