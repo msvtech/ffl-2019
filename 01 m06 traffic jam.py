f70ad3302c5c83de048ea1474b87ba5a112e75b1
Matt Van Winkle <matt@msvtechnology.com>
2019-09-28 16:30:32 -0700

    2019-09-28

--- 01 m06 traffic jam.py
+++ 01 m06 traffic jam.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env micropython
 
 from ev3dev2.motor import LargeMotor, MediumMotor, SpeedPercent, MoveTank, OUTPUT_A, OUTPUT_B, OUTPUT_C # , OUTPUT_D
-# from ev3dev2.sensor.lego import GyroSensor, ColorSensor, TouchSensor
+from ev3dev2.sensor.lego import TouchSensor #,  GyroSensor, ColorSensor
 # from ev3dev2.led import Leds
 import time
 # from math import cos, radians, degrees
@@ -13,19 +13,50 @@ front_motor = MediumMotor(OUTPUT_C)
 # top_motor = MediumMotor(OUTPUT_D)
 # gs = GyroSensor()
 # leds = Leds()
+ts = TouchSensor();
 
 ratio_degrees_to_inches = 360 / 8.44
-rotate = 188.0 / 90.0
+rotate = 135.0 / 90.0
 
-# Mission 6 - Traffic Jam
-# front_motor.on(speed=SpeedPercent(-5))
-tank_drive.on_for_degrees(SpeedPercent(100), SpeedPercent(100), ratio_degrees_to_inches * 20, brake=True)
-time.sleep(0.7786884052)
-front_motor.on(speed=SpeedPercent(100))
-time.sleep(0.5)
-tank_drive.on_for_degrees(SpeedPercent(100), SpeedPercent(100), ratio_degrees_to_inches * 4, brake=True)
-front_motor.off()
+def Crane():
+    # ####################################
+    # Mission 02 - Crane Part 1
+    # ####################################
+
+    # Drive to Crane
+    front_motor.on_for_degrees(speed=SpeedPercent(50), degrees=90, brake=True)
+    tank_drive.on_for_degrees(SpeedPercent(30), SpeedPercent(30), ratio_degrees_to_inches * 11, brake=True)
+    tank_drive.on_for_degrees(SpeedPercent(30), SpeedPercent(-30), rotate * 90)
+    tank_drive.on_for_degrees(SpeedPercent(30), SpeedPercent(30), ratio_degrees_to_inches * 20.5, brake=True)
+
+    # Drop attachment to lower crane lever
+    front_motor.on_for_degrees(speed=SpeedPercent(-10), degrees=45, brake=True)
+    tank_drive.on_for_degrees(SpeedPercent(10), SpeedPercent(10), ratio_degrees_to_inches * -5, brake=True)
+
+    # Drive backwards to get in position for Traffic Jam
+    tank_drive.on_for_degrees(SpeedPercent(30), SpeedPercent(30), ratio_degrees_to_inches * -14.5, brake=True)
+    tank_drive.on_for_degrees(SpeedPercent(-30), SpeedPercent(30), rotate * 100)
 
 
-# Mission 02 - Crane Part 1
-# front_motor.on_for_degrees(speed=SpeedPercent(-40), degrees=90, brake=True)
+def Traffic():
+    # ####################################
+    # Mission 6 - Traffic Jam
+    # ####################################
+
+    front_motor.on(speed=SpeedPercent(-5))
+    time.sleep(0.25)
+    tank_drive.on_for_degrees(SpeedPercent(75), SpeedPercent(75), ratio_degrees_to_inches * 18, brake=True)
+    time.sleep(0.25)
+    front_motor.on(speed=SpeedPercent(75))
+    time.sleep(0.75)
+    tank_drive.on_for_degrees(SpeedPercent(100), SpeedPercent(100), ratio_degrees_to_inches * 8, brake=True)
+    front_motor.off()
+    #tank_drive.on_for_degrees(SpeedPercent(30), SpeedPercent(30), ratio_degrees_to_inches * 4, brake=True)
+    time.sleep(0.5)
+    tank_drive.on_for_degrees(SpeedPercent(100), SpeedPercent(100), ratio_degrees_to_inches * -50, brake=True)
+
+# Run Program
+while not ts.value():
+    time.sleep(0.1)
+Crane()
+Traffic()
--- sandbox/introTeam.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-from ev3dev.ev3 import *
-import os
-import time
-
-sound = Sound()
-sound.speak('Welcome to the Sage Canyon Lego Builders. We are Sara, Pallavi, Isaac, Dylan, Droov and Kiran. We are going to kick your butt! Lets go!')
