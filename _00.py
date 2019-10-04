87fe99936384ac360117ff4347f9034a609e6dc3
Matt Van Winkle <matt@msvtechnology.com>
2019-10-03 17:34:06 -0700

    2019 10 03

--- _00.py
+++ _00.py
@@ -1,15 +1,45 @@
 #!/usr/bin/env micropython
 import time
-from _01_m06_traffic_jam import Crane, Traffic
 from utility import wait_for_button_with_message
+
+from m01_elevated_places            import m01_ElevatedPlaces
+from m02_crane                      import m02_Crane_Part1, m02_Crane_Part2
+from m06_traffic_jam                import m06_TrafficJam
+from m07_swing                      import m07_Swing
+from m10_steel_construction         import m10_SteelConstruction
+from m11_innovative_architecture    import m11_InnovativeArchitecture
+from m12_13_white_circle            import m12_WhiteCircle
+from m12_13_beige_circle            import m12_BeigeCircle
+from m12_13_red_circle              import m12_RedCircle
 
 def main():
 
-    wait_for_button_with_message('Crane')
-    Crane()
+    wait_for_button_with_message('Crane 1')
+    m02_Crane_Part1()
 
     wait_for_button_with_message('Traffic')
-    Traffic()
+    m06_TrafficJam()
+
+    wait_for_button_with_message('Swing')
+    m07_Swing()
+
+    wait_for_button_with_message('Crane 2')
+    m02_Crane_Part2()
+
+    wait_for_button_with_message('White Circle')
+    m12_WhiteCircle()
+
+    wait_for_button_with_message('Beige Circle')
+    m12_BeigeCircle()
+
+    wait_for_button_with_message('Red Circle')
+    m12_RedCircle()
+
+    wait_for_button_with_message('Architecture')
+    m11_InnovativeArchitecture()
+
+    wait_for_button_with_message('Elevated Places')
+    m01_ElevatedPlaces()
 
 if __name__ == '__main__':
     main()
---
+++ m02_crane.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env micropython
+
+from ev3dev2.motor import LargeMotor, MediumMotor, SpeedPercent, MoveTank, OUTPUT_A, OUTPUT_B, OUTPUT_C # , OUTPUT_D
+from ev3dev2.sensor.lego import TouchSensor #,  GyroSensor, ColorSensor
+# from ev3dev2.led import Leds
+import time
+# from math import cos, radians, degrees
+# import os
+# import sys
+
+tank_drive = MoveTank(OUTPUT_A, OUTPUT_B)
+front_motor = MediumMotor(OUTPUT_C)
+# top_motor = MediumMotor(OUTPUT_D)
+# gs = GyroSensor()
+# leds = Leds()
+ts = TouchSensor()
+
+ratio_degrees_to_inches = 360 / 8.44
+rotate = 135.0 / 90.0
+
+def m02_Crane_Part1():
+    # ####################################
+    # Mission 02 - Crane Part 1
+    # ####################################
+
+    # Drive to Crane
+    front_motor.on_for_degrees(speed=SpeedPercent(50), degrees=90, brake=True)
+    tank_drive.on_for_degrees(SpeedPercent(30), SpeedPercent(30), ratio_degrees_to_inches * 11, brake=True)
+    tank_drive.on_for_degrees(SpeedPercent(30), SpeedPercent(-30), rotate * 90)
+    tank_drive.on_for_degrees(SpeedPercent(30), SpeedPercent(30), ratio_degrees_to_inches * 20.5, brake=True)
+
+    # Drop attachment to lower crane lever
+    front_motor.on_for_degrees(speed=SpeedPercent(-10), degrees=45, brake=True)
+    tank_drive.on_for_degrees(SpeedPercent(10), SpeedPercent(10), ratio_degrees_to_inches * -5, brake=True)
+
+    # Drive backwards to get in position for Traffic Jam
+    tank_drive.on_for_degrees(SpeedPercent(30), SpeedPercent(30), ratio_degrees_to_inches * -14.5, brake=True)
+    tank_drive.on_for_degrees(SpeedPercent(-30), SpeedPercent(30), rotate * 100)
+
+def m02_Crane_Part2():
+    # ####################################
+    # Mission 02 - Crane Part 2
+    # ####################################
+    front_motor.on_for_degrees(speed=SpeedPercent(-40), degrees=90, brake=True)
