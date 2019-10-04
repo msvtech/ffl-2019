bfe06384578e7c0a8057635435af163377439a2b
Matt Van Winkle <matt@msvtechnology.com>
2019-10-03 17:58:21 -0700

    2019 10 03

--- _00.py
+++ _00.py
@@ -6,10 +6,11 @@ from missions import m01_ElevatedPlaces
 from missions import m02_Crane_Part1, m02_Crane_Part2
 from missions import m06_TrafficJam
 from missions import m07_Swing
+from missions import m08_Elevator
 from missions import m10_SteelConstruction
 from missions import m11_InnovativeArchitecture
 from missions import m12_WhiteCircle
-from missions import m12_BeigeCircle
+from missions import m12_BeigeCircle, m12_BeigeCircleStacked
 from missions import m12_RedCircle
 
 def main():
@@ -17,8 +18,9 @@ def main():
     wait_for_button_with_message('Traffic');         m06_TrafficJam()
     wait_for_button_with_message('Swing');           m07_Swing()
     wait_for_button_with_message('Crane 2');         m02_Crane_Part2()
+    wait_for_button_with_message('Elevator');        m08_Elevator()
     wait_for_button_with_message('White Circle');    m12_WhiteCircle()
-    wait_for_button_with_message('Beige Circle');    m12_BeigeCircle()
+    wait_for_button_with_message('Beige Circle');    m12_BeigeCircleStacked()
     wait_for_button_with_message('Red Circle');      m12_RedCircle()
     wait_for_button_with_message('Architecture');    m11_InnovativeArchitecture()
     wait_for_button_with_message('Elevated Places'); m01_ElevatedPlaces()
